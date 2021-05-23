56a935a8d07d7194d7ed2b3df5ed73d340575622
wxyzcctv <2846629226@qq.com>
2021-05-23 12:30:50 +0800

    根据语法树实现语法分析

---
+++ 5/LexParser.js
@@ -0,0 +1,119 @@
+class XRegExp {
+    constructor(source, flag, root = "root") {
+        this.table = new Map();
+        this.regexp = new RegExp(this.compileRegExg(source, root, 0).source, flag)
+    }
+    compileRegExg(source, name, start) {
+        if (source[name] instanceof RegExp) {
+            return {
+                source: source[name].source,
+                length: 0
+            }
+        }
+        let length = 0;
+        let regexp = source[name].replace(/\<([^>]+)\>/g, (str, $1) => {
+            this.table.set(start + length, $1)
+            // this.table.set($1, start + length)
+
+            ++length;
+
+            let r = this.compileRegExg(source, $1, start + length)
+
+            length += r.length;
+            return "(" + r.source + ")"
+        })
+        return {
+            source: regexp,
+            length: length
+        };
+    }
+    exec(string) {
+        let r = this.regexp.exec(string)
+        for (let i = 1; i < r.length; i++) {
+            const element = r[i];
+            // 如果r[i]不为undefined时
+            if (r[i] !== void 0) {
+                r[this.table.get(i - 1)] = r[i];
+            }
+        }
+        return r
+    }
+    get lastIndex() {
+        return this.regexp.lastIndex
+    }
+    set lastIndex(value) {
+        return this.regexp.lastIndex = value
+    }
+}
+
+export function* scan(str) {
+    let regexp = new XRegExp({
+        InputElement: "<Whitespace>|<LineTerminator>|<Comments>|<Token>",
+        Whitespace: / /,
+        LineTerminator: /\n/,
+        Comments: /\/\*(?:[^*]|\*[^\/])*\*\/|\/\/[^\n]*/,
+        Token: "<Literal>|<Keywords>|<Identifier>|<Punctuator>",
+        Literal: "<NumericLiteral>|<BooleanLiteral>|<StringLiteral>|<NullLiteral>",
+        NumericLiteral: /(?:[1-9][0-9]*|0)(?:\.[0-9]*)?|\.[0-9]+/,
+        BooleanLiteral: /true|false/,
+        StringLiteral: /\"(?:[^"\n]|\\[\s\S])*\"|\'(?:[^'\n]|\\[\s\S])*\'/,
+        NullLiteral: /null/,
+        Identifier: /[a-zA-z_$][a-zA-Z0-9_$]*/,
+        Keywords: /if|else|for|function|let|var/,
+        Punctuator: /\,|\+|\?|\:|\{|\}|\.|\(|\=|\<|\+\+|\=\=\=|\=\>|\*|\)|\[|\]|;/,
+    }, "g", "InputElement");
+
+    // 正则中[\s\S]表示任意字符,Token中Keywords一定是在Identifier之前，在前括号后加入?:表示的是非捕获分组，即只匹配，不保存
+    while (regexp.lastIndex < str.length) {
+        let r = regexp.exec(str)
+
+        if (r.Whitespace) {
+
+        } else if (r.LineTerminator) {
+
+        } else if (r.Comments) {
+
+        } else if (r.NumericLiteral) {
+            yield {
+                type: "NumericLiteral",
+                value: r[0]
+            }
+        } else if (r.BooleanLiteral) {
+            yield {
+                type: "BooleanLiteral",
+                value: r[0]
+            }
+        } else if (r.StringLiteral) {
+            yield {
+                type: "StringLiteral",
+                value: r[0]
+            }
+        } else if (r.NullLiteral) {
+            yield {
+                type: "NullLiteral",
+                value: null
+            }
+        } else if (r.Identifier) {
+            yield {
+                type: "Identifier",
+                name: r[0]
+            }
+        } else if (r.Keywords) {
+            yield {
+                type: r[0]
+            }
+        } else if (r.Punctuator) {
+            yield {
+                type: r[0]
+            }
+        } else {
+            throw new Error("unexpected token " + r[0])
+        }
+
+        if (!r[0].length)
+            break;
+    }
+    yield {
+        type: "EOF"
+    }
+}
--- 5/SyntaxParser.js
+++ 5/SyntaxParser.js
@@ -1,3 +1,5 @@
+import { scan } from './LexParser.js'
+
 let syntax = {
     Program: [["StatementList"]],
     StatementList: [
@@ -14,12 +16,16 @@ let syntax = {
         ["if", "(", "Expression", ")", "Statement"]
     ],
     VariableDeclaration: [
-        ["var", "Identifier"]
+        ["var", "Identifier", ";"],
+        ["let", "Identifier", ";"]
     ],
     FuntionDeclaration: [
         ["function", "Identifier", "(", ")", "{", "StatementList", "}"]
     ],
     ExpressionStatement: [
+        ["Expression", ";"]
+    ],
+    Expression: [
         ["AdditiveExpression"]
     ],
     AdditiveExpression: [
@@ -41,3 +47,78 @@ let syntax = {
         ["Number"]
     ]
 }
+
+let hash = {
+
+}
+
+function closure(state) {
+    hash[JSON.stringify(state)] = state
+    let queue = [];
+    // 遍历传入的state对象,将传入的state对象中的key存入queue队列中
+    for (const symbol in state) {
+        queue.push(symbol);
+    }
+    // 循环遍历整个队列
+    while (queue.length) {
+        let symbol = queue.shift();
+        // 队列出队得到队首
+        // 如果队首在syntax中,就遍历syntax中的该语法结构
+        if (syntax[symbol]) {
+            for (let rule of syntax[symbol]) {
+                // 遍历过程中,如果syntax中的语法结构的首位不在state中就将该语法结构放入到队列中,如果存在,就将该语法结构的值设为true
+                if (!state[rule[0]]) {
+                    queue.push(rule[0])
+                }
+                let current = state;
+                for (const part of rule) {
+                    if (!current[part]) {
+                        current[part] = {}
+                    }
+                    current = current[part]
+                }
+                current.$reduceType = symbol;
+                current.$reduceSate = state;
+            }
+        }
+    }
+    for (let symbol in state) {
+        if (symbol.match(/^$/)) {
+            return;
+        }
+        if (hash[JSON.stringify(state[symbol])]) {
+            state[symbol] = hash[JSON.stringify(state[symbol])]
+        } else {
+            closure(state[symbol])
+        }
+    }
+}
+let end = {
+    $isEnd: true
+}
+let start = {
+    "Program": end
+}
+
+closure(start)
+
+let source = `
+    let a;
+`
+function parse(source) {
+    let state = start;
+    for (const symbol of scan(source)) {
+        // 此处的symbol为terminal symbol
+        if (symbol.type in state) {
+            state = state[symbol.type]
+        } else {
+            if (state.$reduceType) {
+                state = state.$reduceSate
+            }
+        }
+        console.log(symbol);
+    }
+
+}
+
+parse(source)
